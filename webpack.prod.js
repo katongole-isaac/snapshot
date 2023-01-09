565d4516cef4dc4190f5aecc3ac2d4987091797f
isaac <katongolelsaac78@gmail.com>
2023-01-09 18:55:51 +0300

    env var config added to webpack.prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require("webpack-merge");
 const webpackConfig = require("./webpack.config");
 const TerserWebpackPlugin = require("terser-webpack-plugin");
+const { DefinePlugin } = require("webpack");
 module.exports = merge(webpackConfig, {
   mode: "production",
   output: {
@@ -15,6 +16,16 @@ module.exports = merge(webpackConfig, {
       new TerserWebpackPlugin({
         parallel: true,
         exclude: /node_modules/,
+      }),
+      new DefinePlugin({
+        "process.env": {
+          REACT_APP_API_SECRET_KEY: JSON.stringify(
+            process.env.REACT_APP_API_SECRET_KEY
+          ),
+          REACT_APP_API_ACCESS_KEY: JSON.stringify(
+            process.env.REACT_APP_API_ACCESS_KEY
+          ),
+        },
       }),
     ],
   },
